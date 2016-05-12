69a3ad9e578781deef3073027cc5c06da4f6e367
lun3322 <lun3322@126.com>
2016-05-12 14:42:22 +0800

    添加jQuery dotdotdot

--- Pm.Web/Pm.Web/App_Start/BundleConfig.cs
+++ Pm.Web/Pm.Web/App_Start/BundleConfig.cs
@@ -6,7 +6,8 @@ namespace Pm.Web {
         // 有关绑定的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.dotdotdot.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
