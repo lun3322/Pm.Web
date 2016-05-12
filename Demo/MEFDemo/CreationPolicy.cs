dcb59b73032c9cbc628f59976b4b82a9b43c7213
lun3322 <lun3322@126.com>
2016-05-12 14:08:18 +0800

    添加一个Demo

---
+++ Demo/MEFDemo/CreationPolicy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEFDemo {
+    [Export]
+    public class CreationPolicyTest {
+        public void Run() {
+            var container = Program.Container;
+            var p1 = container.GetExport<Policy1>();
+            var p2 = container.GetExport<Policy2>();
+            p1?.Value.Test();
+            p2?.Value.Test();
+            p2?.Value.Test();
+        }
+    }
+
+    [Export]
+    public class Policy1 {
+        [Import(typeof(IPolicySub), RequiredCreationPolicy = CreationPolicy.Shared)]
+        private IPolicySub PolicySub { get; set; }
+
+        public void Test() {
+            var num = PolicySub.GetNumber();
+            Console.WriteLine(num);
+        }
+    }
+
+    [Export]
+    public class Policy2 {
+        [Import(typeof(IPolicySub), RequiredCreationPolicy = CreationPolicy.Shared)]
+        private IPolicySub PolicySub { get; set; }
+
+        public void Test() {
+            var num = PolicySub.GetNumber();
+            Console.WriteLine(num);
+        }
+    }
+
+    public interface IPolicySub {
+        int GetNumber();
+    }
+
+    [Export(typeof(IPolicySub))]
+    public class PolicySub : IPolicySub {
+        public int Number = 0;
+        public int GetNumber() {
+            Number++;
+            return Number;
+        }
+    }
+}
---
+++ Demo/MEFDemo/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEFDemo {
+    class Program {
+        // public static AggregateCatalog Catalog;
+        public static CompositionContainer Container;
+
+        static void Main(string[] args) {
+            var manager = new Manager();
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly()));
+            Container = new CompositionContainer(catalog);
+            Container.ComposeParts(manager);
+
+            manager.ShowOne();
+            manager.ShowMany();
+            var managerCtor = Container.GetExport<ManagerCtor>();
+            if (managerCtor != null) {
+                managerCtor.Value.ShowConstructor();
+            }
+
+            var export = Container.GetExport<Manager>();
+            export?.Value.ShowOne();
+
+            Console.WriteLine("-----------------------");
+
+            var policy = Container.GetExport<CreationPolicyTest>();
+            policy?.Value.Run();
+        }
+    }
+
+    [Export]
+    public class ManagerCtor {
+        private readonly IConstructorSub _constructorSub;
+
+        public ManagerCtor() { }
+
+        [ImportingConstructor]
+        public ManagerCtor(IConstructorSub constructorSub) {
+            _constructorSub = constructorSub;
+        }
+
+        public void ShowConstructor() {
+            Console.WriteLine("constructorSub.GetName() {0}", _constructorSub.GetName());
+        }
+    }
+
+    [Export]
+    public class Manager {
+        [Import(typeof(ISub))]
+        private ISub Isub { get; set; }
+
+        [ImportMany(typeof(IManySub))]
+        private IEnumerable<Lazy<IManySub>> ImanySub { get; set; }
+
+        public void ShowOne() {
+            Console.WriteLine("Isub.GetName() {0}", Isub.GetName());
+        }
+
+        public void ShowMany() {
+            foreach (var item in ImanySub) {
+                Console.WriteLine("item.GetName() {0}", item.Value.GetName());
+            }
+        }
+    }
+
+    public interface ISub {
+        string GetName();
+    }
+
+    [Export(typeof(ISub))]
+    public class SubOne : ISub {
+        public string ModelName = "SubOne";
+
+        public string GetName() {
+            return ModelName;
+        }
+    }
+
+    public interface IManySub {
+        string GetName();
+    }
+
+    [Export(typeof(IManySub))]
+    public class ManySubOne : IManySub {
+        public string ModelName = "ManySubOne";
+
+        public string GetName() {
+            return ModelName;
+        }
+    }
+
+    [Export(typeof(IManySub))]
+    public class ManySubTwo : IManySub {
+        public string ModelName = "ManySubTwo";
+
+        public string GetName() {
+            return ModelName;
+        }
+    }
+
+    [Export(typeof(IManySub))]
+    public class ManySubThree : IManySub {
+        public string ModelName = "ManySubThree";
+
+        public string GetName() {
+            return ModelName;
+        }
+    }
+
+    public interface IConstructorSub {
+        string GetName();
+    }
+
+    [Export(typeof(IConstructorSub))]
+    public class ConstructorSub : IConstructorSub {
+        public string ModelName = "ConstructorSub";
+
+        public string GetName() {
+            return ModelName;
+        }
+    }
+}
