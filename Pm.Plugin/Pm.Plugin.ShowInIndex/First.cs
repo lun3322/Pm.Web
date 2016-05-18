a50ba8b2db3344f4fb98430db6f57b78e11d0016
lun3322 <lun3322@126.com>
2016-05-18 18:00:30 +0800

    为了连击

--- Pm.Plugin/Pm.Plugin.ShowInIndex/First.cs
+++ Pm.Plugin/Pm.Plugin.ShowInIndex/First.cs
@@ -10,7 +10,7 @@ namespace Pm.Plugin.ShowInIndex {
     [Export(typeof(IIndexBlock))]
     public class First : IIndexBlock {
         public string GetText() {
-            return "全球最大的中文搜索引擎、致力于让网民更便捷地获取信息，找到所求。百度超过千亿的中文网页数据库，可以瞬间找到相关的搜索结果。";
+            return "全球最大的中文搜索引擎、致力于让网民更便捷地获取信息，找到所求。百度超过千亿的中文网页数据库，可以瞬间找到相关的搜索结果。111111";
         }
 
         public string GetTitle() {
